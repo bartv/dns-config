0259c3d3e7954e107b79b550c92873d0c6d3bad2
Bart Vanbrabant <bart.vanbrabant@zoeloelip.be>
2007-07-22 11:19:57 +0200

    Add setup script

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+        name = "dns-config",
+        version = "0.2",
+        packages = find_packages('src'),
+        package_dir = {'':'src'},
+
+        author = "Bart Vanbrabant",
+        author_email = "bart.vanbrabant@zoeloelip.be",
+        url = "http://bart.ulyssis.org/hg/dns-config",
+)
