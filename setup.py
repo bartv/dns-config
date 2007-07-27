3c2d14664d8ff84a701d04ad33b383e2b64e72d4
Bart Vanbrabant <bart.vanbrabant@zoeloelip.be>
2007-07-27 14:19:59 +0200

    Improve setup.py

--- setup.py
+++ setup.py
@@ -5,8 +5,14 @@ setup(
         version = "0.2",
         packages = find_packages('src'),
         package_dir = {'':'src'},
+        description = "Tool to generate dns configuration from ldap",
+        license = "MIT",
 
         author = "Bart Vanbrabant",
         author_email = "bart.vanbrabant@zoeloelip.be",
         url = "http://bart.ulyssis.org/hg/dns-config",
+
+        entry_points = { 
+            'console_scripts' : ['dnsconfig = dnsconfig:main']
+        }
 )
